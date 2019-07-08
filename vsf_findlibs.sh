0be28c8c124f843d11696e8b2a88799ebf75ed91
Peter Simons <psimons@suse.com>
2019-07-08 09:41:12 +0000

    build against OpenSSL 1.1.x [bsc#1042673]

--- vsf_findlibs.sh
+++ vsf_findlibs.sh
@@ -72,7 +72,7 @@ locate_library /usr/shlib/librt.so && echo "-lrt";
 locate_library /usr/lib/libsendfile.so && echo "-lsendfile";
 
 # OpenSSL
-if find_func SSL_library_init ssl.o; then
+if find_func SSL_CTX_new ssl.o; then
   echo "-lssl -lcrypto";
 fi
 
